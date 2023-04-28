f3c2bda64cc13081212f65f535e724a6fa02cf63
yash-2523 <yash.d1@ahduni.edu.in>
2023-04-28 12:28:17 +0530

    arb-configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,27 +4,32 @@ require('hardhat-docgen');
 /** @type import('hardhat/config').HardhatUserConfig */
 const dotenv = require("dotenv");
 dotenv.config({ path: __dirname + "/.env" });
-const PRIVATE_KEY = "Your private key";
-const API_KEY = "YOUR API KEY";
+const PRIVATE_KEY = "Your private key with 0x prefix";
 
 module.exports = {
   networks: {
-    bscTestnet: {
-      url: "https://bsctestapi.terminet.io/rpc",
-      chainId: 97,
-      gasPrice: 20000000000,
-      accounts: [`0x${PRIVATE_KEY}`]
-    },
-    bsc: {
-      url: "https://bsc-dataseed.binance.org/",
-      chainId: 56,
-      gasPrice: 20000000000,
-      accounts: [`0x${PRIVATE_KEY}`]
-    },
-    avax: {
-      url: 'https://api.avax.network/ext/bc/C/rpc',
+    arbitrum_testnet: {
+      url: "Alchmey arbitrum testnet url",
+      chainId: 421613,
       gasPrice: 225000000000,
-      chainId: 43114,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
+    arbitrumOne: {
+      url: "Alchmey arbitrum mainnet url",
+      chainId: 42161,
+      gasPrice: 225000000000,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
+    polygonMumbai: {
+      url: "Alchmey polygon mainnet url",
+      chainId: 80001,
+      gasPrice: 225000000000,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
+    polygon: {
+      url: "Alchmey polygon mainnet url",
+      chainId: 137,
+      gasPrice: 225000000000,
       accounts: [`0x${PRIVATE_KEY}`]
     }
   },
@@ -37,6 +42,21 @@ module.exports = {
     }
   },
   etherscan: {
-    apiKey: API_KEY
+    apiKey:{
+      "arb_testnet": "Arbitrum api key",
+      "arbitrumOne": "Arbitrum api key",
+      "polygonMumbai": "Polygon api key",
+      "polygon": "Polygon api key",
+    },
+    customChains: [
+      {
+        network: "arb_testnet",
+        chainId: 421613,
+        urls: {
+          apiURL: "https://api-goerli.arbiscan.io/api",
+          browserURL: "https://goerli.arbiscan.io/"
+        }
+      }
+    ]
   }
 };
